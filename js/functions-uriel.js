56e746cf8ec0f92e7c6e0bf8ec78ff50f1250496
Juan <jp7patrick@gmail.com>
2019-03-23 03:57:24 -0600

    *changed* stlyes-patron

--- js/functions-uriel.js
+++ js/functions-uriel.js
@@ -6,3 +6,13 @@ function menuButton(){
 function test(){
     alert("si jala");
 }
+
+function startDiagnosis(){
+    var instrucciones = document.getElementById("cat");
+    var instrucciones2 = document.getElementById("cat2");
+    instrucciones.style.display="none";
+    instrucciones2.style.display="none";
+
+    var subtitulo = document.getElementById("subTitle");
+    subtitulo.style.display="initial";
+}
